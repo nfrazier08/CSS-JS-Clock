7366f635470b2d5d26bbb83108793a0d11efc995
neliza <nicolasaelf64@gmail.com>
2019-02-09 14:04:49 -0700

    adjust degrees for hours

--- script.js
+++ script.js
@@ -11,40 +11,40 @@ window.onload = function(){
         //Let's grab the date
         const now = new Date();
 
+        //SECONDS!
         //This is giving the seconds of the current minute- but we need it in degrees so 
         //the app knows by how much to move the second hand
         const seconds = now.getSeconds();
-
-        //get minutes just like we did seconds
-        const minutes = now.getMinutes();
-
-        //get hours just like we did seconds
-        const hours = now.getHours();
-
         //Divide by 60 because there are 60 seconds in a minute. This will also give us the %
         //We mult by 360 to get the degrees of circle
-        const secondDegrees = ((seconds/60) * 360);
-
-        //Get minutes in degrees
-        const minuteDegrees = ((minutes/60) * 360);
-
-        //Get hours in degrees
-        const hourDegrees = ((hours/60) * 360);
-
+        const secondDegrees = ((seconds/60) * 360) + 90;
         //We need to apply styles to the second hand
-        secondHand.style.transform = `rotate(${secondDegrees}deg)`
+        secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+
+        //MINUTES!
+        //Get minutes
+        const minutes = now.getMinutes();
+        //Get minutes in degrees
+        //NOTE: The default here is Left to Right. In our transform (CSS) we added 90 degrees
+        //to make the default top to bottom. So, we have to add 90 degrees back here to offset the change made
+        const minuteDegrees = ((minutes/60) * 360) + 90;
         //Apply styles to minutes
-        minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+        minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
+
+        //HOURS!
+        //Get Hours
+        const hours = now.getHours();
+        //Get hours in degrees
+        const hourDegrees = ((hours/12) * 360) + 90;
         //Apply styles to hours
         hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 
-        //console.log(seconds);
-        //console.log(minutes);
-        //console.log(hours);
         console.log(now);
+        
+       
     }
 
     //We need the setDate fxn to run every second
